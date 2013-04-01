bd6f9845a47b159e94cf05acd17b78bcdb457c37
Paul-André Duchesne <Paul-Andre.Duchesne@naturalsciences.be>
2013-04-01 04:09:04 +0200

    3 Pages ready and 1 nearly at static form
    All these pages are (nearly) ready at static form:
    - Home page
    - Agreements
    - Team
    - References: this one has to be worked for list of realisations
    
    Slideshow done and ready for home page

---
+++ scripts/slideshow-slides-lists.js
@@ -0,0 +1,138 @@
+//    Array of slides for the first image
+var slides_list_1 = Array(
+    {
+        url: '/images/mini.jpg',
+    },
+    {
+        url: '/images/mini_grey.jpg',
+    }
+);
+
+//    Array of slides for the second image
+var slides_list_2 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Array of slides for the third image
+var slides_list_3 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Array of slides for the fourth image
+var slides_list_4 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Array of slides for the fifth image
+var slides_list_5 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Array of slides for the sixth image
+var slides_list_6 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Array of slides for the seventh image
+var slides_list_7 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Array of slides for the eighth image
+var slides_list_8 = Array(
+  {
+    url: '/images/mini.jpg',
+  },
+  {
+    url: '/images/mini_grey.jpg',
+  }
+);
+
+//    Add the slides on initialisation
+$('#slideshow-container-1').rsfSlideshow(
+  {
+    slides: slides_list_1,
+    interval: 40,
+    autostart: false,
+  }
+);
+//    Add the slides on initialisation
+$('#slideshow-container-2').rsfSlideshow(
+  {
+    slides: slides_list_2,
+    interval: 40,
+    autostart: false,
+  }
+);//    Add the slides on initialisation
+$('#slideshow-container-3').rsfSlideshow(
+  {
+    slides: slides_list_3,
+    interval: 40,
+    autostart: false,
+  }
+);//    Add the slides on initialisation
+$('#slideshow-container-4').rsfSlideshow(
+  {
+    slides: slides_list_4,
+    interval: 40,
+    autostart: false,
+  }
+);//    Add the slides on initialisation
+$('#slideshow-container-5').rsfSlideshow(
+  {
+    slides: slides_list_5,
+    interval: 40,
+    autostart: false,
+  }
+);//    Add the slides on initialisation
+$('#slideshow-container-6').rsfSlideshow(
+  {
+    slides: slides_list_6,
+    interval: 40,
+    autostart: false,
+  }
+);//    Add the slides on initialisation
+$('#slideshow-container-7').rsfSlideshow(
+  {
+    slides: slides_list_7,
+    interval: 40,
+    autostart: false,
+  }
+);//    Add the slides on initialisation
+$('#slideshow-container-8').rsfSlideshow(
+  {
+    slides: slides_list_8,
+    interval: 40,
+    autostart: false,
+  }
+);
